4bdb80658717271330266a79e2e5aaf9e7426f47
Oscar <53874145+ozqr20@users.noreply.github.com>
2022-11-02 21:35:15 -0400

    query resolvers.js

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,7 +3,20 @@ const { Users, Books } = require('../models');
 const { signToken } = require('../utils/auth');
 
 const resolvers = {
+    Query: {
+        me: async (parent, args, context) => {
+            if (context.user) {
+            const userDB = await User.findOne({ _id: context.user._id })
+                .select('-__v -password')
+                return userDB;
+            }
+            throw new AuthenticationError("Not Logged in");
+        }
+    },
 
-}
+    Mutation: {
+
+    }
+};
 
 module.exports = resolvers;
